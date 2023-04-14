b5bee42567c417d90e8c7e3163e9c355414df1b5
Luca <gabadadzeluca@gmail.com>
2023-04-15 00:08:58 +0400

    initial commit

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,12 @@
+export default function Search() {
+  return (
+    <div>
+      <form>
+        <input type='text'/>
+        <button type='submit'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
