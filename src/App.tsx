b4a782d3dae0b7966f37b6f35f1ddf1231bca0c5
Luca <gabadadzeluca@gmail.com>
2023-04-20 01:35:46 +0400

    finish project

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 import Search from './components/search/Search';
+import DownloadLink from './components/download/DownloadLink';
+import {urlResultObject, urlResultInterface} from './components/download/util/urlResultObject';
 
 function App() {
+  const[urlResult, setUrlResult] = useState<urlResultInterface|null>(urlResultObject);
+  // const[downloadLink, setDownloadLink] = useState<string|undefined>('');
+  
   return (
     <div className='App'>
-      <Search />
+      <div className='Container'>
+        <Search 
+          // setDownloadLink={setDownloadLink}
+          setUrlResult={setUrlResult}
+        />
+        {urlResult?.title ? 
+          (<DownloadLink urlResult={urlResult}/>)
+          : 
+          null
+        }
+      </div>
     </div>
   );
 }
