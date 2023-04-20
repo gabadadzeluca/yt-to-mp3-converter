51684c7de6841be2f74735623d341eab8f37b08f
Luca <gabadadzeluca@gmail.com>
2023-04-20 14:34:08 +0400

    alter urlResult objet

--- src/utls/urlResultObject.tsx
+++ src/utls/urlResultObject.tsx
@@ -3,14 +3,12 @@ interface urlResultInterface {
   'title': string,
   'progress'?: number,
   'duration'?: number,
-  'status:': string
 }
 const urlResultObject:urlResultInterface = {
   'link': '',
   'title': '',
   'progress': 0,
   'duration': 0,
-  'status:': ''
 }
 
 export {urlResultObject};
