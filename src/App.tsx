a1672cd27a5892b5b5dd1c5ef66dddab7a3c605b
Luca <gabadadzeluca@gmail.com>
2023-04-24 22:42:48 +0400

    add new state variable for displaying the message

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import './App.css';
 import Search from './components/search/Search';
 import DownloadLink from './components/download/DownloadLink';
 import {urlResultObject, urlResultInterface} from './utls/urlResultObject';
+import ErrorComponent from './components/error/ErrorComponent';
 
 function App() {
   const[urlResult, setUrlResult] = useState<urlResultInterface|null>(urlResultObject);
-  
+  const[requestLimitError, setRequestLimitError] = useState<boolean>(false);
   return (
     <div className='App'>
       <div className='Container'>
         <Search 
           setUrlResult={setUrlResult}
+          setRequestLimitError={setRequestLimitError}
         />
         {urlResult?.title ? 
           (<DownloadLink urlResult={urlResult}/>)
           : 
+          null
+        }
+        { requestLimitError ? 
+          (<ErrorComponent />)
+          :
           null
         }
       </div>
